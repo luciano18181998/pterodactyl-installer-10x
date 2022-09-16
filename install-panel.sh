c6e88c1b5518ad815159b69b2707aa8430008290
SrStuxBR <50122626+SrStuxBR@users.noreply.github.com>
2022-09-15 23:04:26 -0300

    Update install-panel.sh

--- install-panel.sh
+++ install-panel.sh
@@ -72,8 +72,8 @@ ASSUME_SSL=false
 CONFIGURE_LETSENCRYPT=false
 
 # download URLs
-PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz"
-GITHUB_BASE_URL="https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/$GITHUB_SOURCE"
+PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/download/v1.8.1/panel.tar.gz"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/srstuxbr/pterodactyl-installer/$GITHUB_SOURCE"
 
 # ufw firewall
 CONFIGURE_UFW=false
