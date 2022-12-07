11bb72ed13ae13a515a07e1a7ed35d51013886c9
SrStuxBR <50122626+SrStuxBR@users.noreply.github.com>
2022-12-06 22:26:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ set -e
 #                                                                           #
 #############################################################################
 
-SCRIPT_VERSION="v1.3.5"
+SCRIPT_VERSION="v1.3.6"
 GITHUB_BASE_URL="https://raw.githubusercontent.com/SrStuxBR/pterodactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
